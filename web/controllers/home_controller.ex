558cfb668928727a69e70c057199a8b9a91fe6b8
Kristaps Erglis <kristaps.erglis@gmail.com>
2017-12-23 15:37:13 +0200

    Render 4 random cities

--- web/controllers/home_controller.ex
+++ web/controllers/home_controller.ex
@@ -1,7 +1,12 @@
 defmodule LvCoatsOfArms.HomeController do
   use LvCoatsOfArms.Web, :controller
 
+  alias LvCoatsOfArms.City
+
   def index(conn, _params) do
-    render conn, "index.html"
+    cities = City.random(4)
+    [random_city] = Enum.take_random(cities, 1)
+
+    render conn, "index.html", cities: cities, random_city: random_city
   end
 end
